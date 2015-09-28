f2ed6ac90e41be97b346be3ae6670a0b1b9d2a61
Daniel Lutziger <daniellutziger@Administrators-MacBook-Pro-2.local>
2015-09-28 09:27:14 +0200

    Initial Commit

---
+++ Blinded/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Blinded
+//
+//  Created by Daniel Lutziger on 28.09.15.
+//  Copyright © 2015 Daniel Lutziger. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
